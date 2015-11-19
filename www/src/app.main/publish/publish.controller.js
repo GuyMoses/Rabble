bcf4683820565bf267e30e30b66885087c6723fe
Ido Ben-Natan <ido.bennatan@gmail.com>
2015-11-19 21:43:23 +0200

    7th commit

--- www/src/app.main/publish/publish.controller.js
+++ www/src/app.main/publish/publish.controller.js
@@ -3,6 +3,7 @@
   function PublishCtrl($scope,$log,$http,$timeout,$location,fileReader, Product){
 
     $scope.product = {};
+    $scope.publishing = false;
 
     $scope.product.imageSrc = 'src/app.assets/img/placeholder.jpg';
     $scope.getFile = function () {
@@ -13,11 +14,13 @@
     };
 
     $scope.Publish = function(){
+      $scope.publishing = true;
       Product.uploadImage($scope.product.title, $scope.product.imageSrc).success(function(data) {
         $log.info(data);
         $scope.product.imageUrl = data.url
         Product.create($scope.product).success(function(data) {
           $log.info(data);
+          $scope.publishing = false;
         });
       });
       //"https://api.backand.com/1/objects/action/products/?name=S3FileUpload&parameters=%7B%22filename%22:%22%22,%22filedata%22:%22%22%7D"
